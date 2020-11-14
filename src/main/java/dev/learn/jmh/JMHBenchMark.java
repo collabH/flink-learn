9acfd34b2301671e7158d4f1c160f09dff74c23f
Shiming Huang <h1261109615@qq.com>
2020-11-14 22:22:12 +0800

    feat: 添加JMH测试

---
+++ src/main/java/dev/learn/jmh/JMHBenchMark.java
@@ -0,0 +1,10 @@
+package dev.learn.jmh;
+
+/**
+ * @fileName: JMHBenchMark.java
+ * @description: JMHBenchMark.java类说明
+ * @author: by echo huang
+ * @date: 2020/11/14 10:21 下午
+ */
+public class JMHBenchMark {
+}
