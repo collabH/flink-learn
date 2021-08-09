de934e7c80324d1d47d6c8085c4182808edfa851
Shimin Huang <40719512+collabH@users.noreply.github.com>
2021-08-09 23:45:01 +0800

    feat: 1.13流批一体区别  1.12+watermark

--- flink-1.13-study/src/main/java/dev/learn/flink/feature/ExecutionModeFeature.java
+++ flink-1.13-study/src/main/java/dev/learn/flink/feature/ExecutionModeFeature.java
@@ -1,7 +1,25 @@
 package dev.learn.flink.feature;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.compress.utils.Lists;
 import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.TimerService;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @fileName: ExecutionMode.java
@@ -10,16 +28,88 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  * @date: 2021/8/8 8:35 下午
  */
 public class ExecutionModeFeature {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setRuntimeMode(RuntimeExecutionMode.BATCH);
-        env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
-        env.setRuntimeMode(RuntimeExecutionMode.STREAMING);
+//        env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
+//        env.setRuntimeMode(RuntimeExecutionMode.STREAMING);
         // 执行DAG不同，批处理可以更多地优化
 
         // 状态后端 1.流模式flink使用状态后端控制状态如何存储如何做ck  2.批处理忽略状态后端，相反，键操作的输入按键分组(使用排序)
         // ，然后依次处理键的所有记录。这允许同时只保留一个键的状态。当移动到下一个键时，给定键的状态将被丢弃。
 
         // 2.处理顺序，流模式无法保证顺序一旦达到就处理除非配合watermark等， 批处理能够保证顺序
+
+        // 3. 处理时间，Batch模式使用处理时间可以注册处理时间timers
+
+        List<Result> inputs = Lists.newArrayList();
+        long ts = System.currentTimeMillis();
+        inputs.add(Result.builder().ts(ts)
+                .id("1").build());
+        inputs.add(Result.builder().ts(ts + 5 * 1000)
+                .id("1").build());
+        inputs.add(Result.builder().ts(ts + 6 * 1000)
+                .id("1").build());
+        inputs.add(Result.builder().ts(ts + 7 * 1000)
+                .id("1").build());
+        inputs.add(Result.builder().ts(ts + 10 * 1000)
+                .id("1").build());
+        inputs.add(Result.builder().ts(ts + 15 * 1000)
+                .id("1").build());
+        DataStreamSource<Result> ds = env.fromCollection(inputs);
+        registerBatchTimer(ds);
+
+        env.execute();
+    }
+
+    private static void registerBatchTimer(DataStreamSource<Result> ds) {
+        ds.keyBy(new KeySelector<Result, String>() {
+            @Override
+            public String getKey(Result result) throws Exception {
+                return result.getId();
+            }
+        }).process(new KeyedProcessFunction<String, Result, String>() {
+            private transient ValueStateDescriptor<Long> longValueStateDescriptor;
+            private transient ValueState<Long> valueState;
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                super.open(parameters);
+                this.longValueStateDescriptor = new ValueStateDescriptor<Long>("timer",
+                        TypeInformation.of(Long.class));
+                valueState = getRuntimeContext().getState(this.longValueStateDescriptor);
+            }
+
+            @Override
+            public void processElement(Result result, Context context, Collector<String> collector) throws Exception {
+                TimerService timerService = context.timerService();
+                Long ts = result.getTs();
+                long timeTrigger = ts + 5000;
+                if (valueState.value() == null || valueState.value() == -1L) {
+                    timerService.registerProcessingTimeTimer(timeTrigger);
+                    valueState.update(timeTrigger);
+                }
+
+            }
+
+            @Override
+            public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
+                super.onTimer(timestamp, ctx, out);
+                Long timer = valueState.value();
+                ctx.timerService().deleteProcessingTimeTimer(timer);
+                valueState.update(-1L);
+                System.out.println("ts:" + timestamp);
+            }
+        });
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    static class Result {
+        private String id;
+        private Long ts;
     }
 }
---
+++ flink-1.13-study/src/main/java/dev/learn/flink/feature/WatermarkerFeature.java
@@ -0,0 +1,42 @@
+package dev.learn.flink.feature;
+
+import org.apache.flink.api.common.eventtime.WatermarkGenerator;
+import org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.shaded.guava18.com.google.common.collect.Maps;
+import org.apache.flink.streaming.api.SimpleTimerService;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.time.Duration;
+import java.util.Properties;
+
+/**
+ * @fileName: WatermarkerFeature.java
+ * @description: WatermarkerFeature.java类说明
+ * @author: huangshimin
+ * @date: 2021/8/9 11:25 下午
+ */
+public class WatermarkerFeature {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<Long> ds = env.fromElements(1L, 2L);
+        ds.assignTimestampsAndWatermarks(WatermarkStrategy.<Long>forBoundedOutOfOrderness(Duration.ofSeconds(10))
+                .withTimestampAssigner((event, timestamp) -> event));
+
+        // 如果存在空闲subtask，1.11之前watermark由于watermark计算机制取所有subtask的最小watermark，空闲subtask会导致其他subtask数据无法正常
+        // 触发，使用withIdleness标识过一段时间没有数据来则标识为空闲数据源
+        ds.assignTimestampsAndWatermarks(WatermarkStrategy.<Long>forBoundedOutOfOrderness(Duration.ofSeconds(10))
+                .withTimestampAssigner((event, timestamp) -> event)
+                .withIdleness(Duration.ofSeconds(10)));
+
+        // 使用kafka自身数据的时间戳
+        FlinkKafkaConsumer<String> kafkaSource = new FlinkKafkaConsumer<>("myTopic", new SimpleStringSchema(),
+                new Properties());
+        kafkaSource.assignTimestampsAndWatermarks(
+                WatermarkStrategy.forBoundedOutOfOrderness(Duration.ofSeconds(20)));
+
+    }
+}
