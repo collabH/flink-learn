4202cc1bb8c8ce9e8a9672e8191700f507ecd864
Shimin Huang <40719512+collabH@users.noreply.github.com>
2021-08-31 21:17:04 +0800

    feat:  flink1.13窗口聚合

---
+++ flink-1.13-study/src/main/java/dev/learn/flink/InterruptDemo.java
@@ -0,0 +1,32 @@
+package dev.learn.flink;
+
+public class InterruptDemo {
+    public static void main(String[] args) throws InterruptedException {
+        //sleepThread睡眠1000ms
+        final Thread sleepThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                super.run();
+            }
+        };
+        //busyThread一直执行死循环
+        Thread busyThread = new Thread() {
+            @Override
+            public void run() {
+                while (true) ;
+            }
+        };
+        sleepThread.start();
+        busyThread.start();
+        sleepThread.interrupt();
+        busyThread.interrupt();
+        while (sleepThread.isInterrupted()) ;
+        System.out.println("sleepThread isInterrupted: " + sleepThread.isInterrupted());
+        System.out.println("busyThread isInterrupted: " + busyThread.isInterrupted());
+    }
+}
--- flink-1.13-study/src/main/java/dev/learn/flink/sql/grammar/SQLGrammarFeature.java
+++ flink-1.13-study/src/main/java/dev/learn/flink/sql/grammar/SQLGrammarFeature.java
@@ -12,16 +12,21 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
  * @date: 2021/8/29 7:02 下午
  */
 public class SQLGrammarFeature {
-    static String expample_sql = "create table test(name string,id int,event_time timestamp_ltz(3),watermark for " +
-            "event_time as event_time - INTERVAL '1' second)with" +
-            "('connector'='datagen')";
+    static String expample_sql = "create table test(name string,id int,age bigint,price int,event_time " +
+            "timestamp_ltz(3)," +
+            "watermark for event_time as event_time - INTERVAL '1' second)with" +
+            "('connector'='datagen', 'fields.id.min'='1', 'fields.id.max'='10','fields.price.min'='1', " +
+            "'fields.price.max'='10','fields.age.kind'='sequence'," +
+            "'fields.age.start'='1','fields.age.end'='100')";
 
     public static void main(String[] args) {
         StreamExecutionEnvironment streamEnv = FlinkEnvUtils.getStreamEnv();
         StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(streamEnv);
 //        with(tableEnvironment);
-        window(tableEnvironment);
+//        window(tableEnvironment);
+        windowAgg(tableEnvironment);
     }
+
 
     /**
      * 动态参数
@@ -29,7 +34,8 @@ public class SQLGrammarFeature {
      * @param tableEnvironment
      */
     private static void sqlHints(StreamTableEnvironment tableEnvironment) {
-        tableEnvironment.getConfig().getConfiguration().set(ConfigOptions.key("table.dynamic-table-options.enabled")
+        tableEnvironment.getConfig().getConfiguration().set(ConfigOptions.key("table.dynamic-table-options" +
+                ".enabled")
                 .booleanType().defaultValue(true), true);
         tableEnvironment.executeSql("select id, name from kafka_table1 /*+ OPTIONS('scan.startup" +
                 ".mode'='earliest-offset','connector.type'='kafka') */");
@@ -49,14 +55,46 @@ public class SQLGrammarFeature {
     private static void window(StreamTableEnvironment tableEnvironment) {
         tableEnvironment.executeSql(expample_sql);
         tableEnvironment.executeSql("desc test").print();
-        String tumbleWindowSQLGroupBy = "select window_start, window_end,sum(id) as count1 from TABLE(TUMBLE(TABLE test," +
+        String tumbleWindowSQLGroupBy = "select window_start, window_end,sum(id) as count1 from TABLE(TUMBLE" +
+                "(TABLE test," +
                 "DESCRIPTOR (event_time),interval '5' second)) GROUP BY window_start, window_end";
-        String hopWindowSQLGroupBy = "select window_start, window_end,sum(id) as count1 from TABLE(HOP(TABLE test," +
-                "DESCRIPTOR (event_time),INTERVAL '1' second,interval '5' second)) GROUP BY window_start, window_end";
+        String hopWindowSQLGroupBy = "select window_start, window_end,sum(id) as count1 from TABLE(HOP(TABLE " +
+                "test," +
+                "DESCRIPTOR (event_time),INTERVAL '1' second,interval '5' second)) GROUP BY window_start, " +
+                "window_end";
         String cumulateWindowSQLGroupBy = "select window_start, window_end,max(id) as count1 from TABLE(CUMULATE" +
                 "(TABLE " +
-                "test,DESCRIPTOR (event_time),INTERVAL '1' second,interval '5' second)) GROUP BY window_start, window_end";
+                "test,DESCRIPTOR (event_time),INTERVAL '1' second,interval '5' second)) GROUP BY window_start, " +
+                "window_end";
         tableEnvironment.executeSql(cumulateWindowSQLGroupBy)
                 .print();
+    }
+
+    private static void windowAgg(StreamTableEnvironment tableEnvironment) {
+        tableEnvironment.executeSql(expample_sql);
+        String tumbleWindowSQLGroupingSets = "select window_start, window_end,id from TABLE" +
+                "(TUMBLE(TABLE test,DESCRIPTOR (event_time),interval '5' second)) GROUP BY window_start, " +
+                "window_end,grouping sets((id))";
+        // rollup会携带全部的pv指标
+        String rollUpSql = "select window_start,window_end,id,count(name) as pv from table" +
+                "(tumble(table" +
+                " " +
+                "test," +
+                "descriptor" +
+                "(event_time)," +
+                "interval '5' second)) group by window_start,window_end,rollup(id)";
+
+        String cubeSql = "select window_start,window_end,window_time,id,sum(price) as sum_price from table" +
+                "(tumble(table" +
+                " " +
+                "test," +
+                "descriptor" +
+                "(event_time)," +
+                "interval '5' second)) group by window_start,window_end,window_time,cube(id)";
+
+
+
+        tableEnvironment.executeSql(cubeSql).print();
+
     }
 }
