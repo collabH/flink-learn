a87f194c02ac16cdeda54019aea87368ec253290
Shimin Huang <40719512+collabH@users.noreply.github.com>
2021-09-01 22:20:49 +0800

    feat[1.13]: flink sql over聚合、分局组合、窗口组合语法

--- flink-1.13-study/src/main/java/dev/learn/flink/sql/grammar/SQLGrammarFeature.java
+++ flink-1.13-study/src/main/java/dev/learn/flink/sql/grammar/SQLGrammarFeature.java
@@ -3,7 +3,11 @@ package dev.learn.flink.sql.grammar;
 import dev.learn.flink.FlinkEnvUtils;
 import org.apache.flink.configuration.ConfigOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import javax.print.DocFlavor;
 
 /**
  * @fileName: SQLGrammarFeature.java
@@ -13,7 +17,7 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
  */
 public class SQLGrammarFeature {
     static String expample_sql = "create table test(name string,id int,age bigint,price int,event_time " +
-            "timestamp_ltz(3)," +
+            "timestamp(3)," +
             "watermark for event_time as event_time - INTERVAL '1' second)with" +
             "('connector'='datagen', 'fields.id.min'='1', 'fields.id.max'='10','fields.price.min'='1', " +
             "'fields.price.max'='10','fields.age.kind'='sequence'," +
@@ -22,9 +26,13 @@ public class SQLGrammarFeature {
     public static void main(String[] args) {
         StreamExecutionEnvironment streamEnv = FlinkEnvUtils.getStreamEnv();
         StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(streamEnv);
+        TableEnvironment batchTableEnvironment = TableEnvironment.create(
+                EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build());
 //        with(tableEnvironment);
 //        window(tableEnvironment);
-        windowAgg(tableEnvironment);
+//        windowAgg(tableEnvironment);
+//        groupFunction(batchTableEnvironment);
+        overAgg(tableEnvironment);
     }
 
 
@@ -93,8 +101,80 @@ public class SQLGrammarFeature {
                 "interval '5' second)) group by window_start,window_end,window_time,cube(id)";
 
 
+        String windowStartSql = "select tumble_start(event_time,INTERVAL '5' second) as wStart,sum(price) as " +
+                "priceC from test group by tumble(event_time,INTERVAL '5' second)";
 
-        tableEnvironment.executeSql(cubeSql).print();
-
+        tableEnvironment.executeSql(windowStartSql).print();
     }
+
+    private static void groupFunction(TableEnvironment tableEnvironment) {
+        String countSql = "select count(id) as pv from test";
+        String distinctSql = "select distinct(id) from test";
+        String groupingSetsSql = "SELECT supplier_id, rating, COUNT(*) AS total\n" +
+                "FROM (VALUES\n" +
+                "    ('supplier1', 'product1', 4),\n" +
+                "    ('supplier1', 'product2', 3),\n" +
+                "    ('supplier2', 'product3', 3),\n" +
+                "    ('supplier2', 'product4', 4))\n" +
+                "AS Products(supplier_id, product_id, rating)\n" +
+                "GROUP BY GROUPING SETS ((supplier_id, rating), (supplier_id), ())";
+
+        // 可以达到和grouping sets（(supplier_id, rating), (supplier_id), ()）的效果
+        String rollupSql="SELECT supplier_id, rating, COUNT(*)\n" +
+                "FROM (VALUES\n" +
+                "    ('supplier1', 'product1', 4),\n" +
+                "    ('supplier1', 'product2', 3),\n" +
+                "    ('supplier2', 'product3', 3),\n" +
+                "    ('supplier2', 'product4', 4))\n" +
+                "AS Products(supplier_id, product_id, rating)\n" +
+                "GROUP BY ROLLUP (supplier_id, rating)";
+
+        String cubeSql="SELECT supplier_id, rating, product_id, COUNT(*)\n" +
+                "FROM (VALUES\n" +
+                "    ('supplier1', 'product1', 4),\n" +
+                "    ('supplier1', 'product2', 3),\n" +
+                "    ('supplier2', 'product3', 3),\n" +
+                "    ('supplier2', 'product4', 4))\n" +
+                "AS Products(supplier_id, product_id, rating)\n" +
+                "GROUP BY CUBE (supplier_id, rating, product_id)";
+
+        // cube三个字段等价于GROUPING SET
+        String equalCubeGroupingSql2="SELECT supplier_id, rating, product_id, COUNT(*)\n" +
+                "FROM (VALUES\n" +
+                "    ('supplier1', 'product1', 4),\n" +
+                "    ('supplier1', 'product2', 3),\n" +
+                "    ('supplier2', 'product3', 3),\n" +
+                "    ('supplier2', 'product4', 4))\n" +
+                "AS Products(supplier_id, product_id, rating)\n" +
+                "GROUP BY GROUPING SETS (\n" +
+                "    ( supplier_id, product_id, rating ),\n" +
+                "    ( supplier_id, product_id         ),\n" +
+                "    ( supplier_id,             rating ),\n" +
+                "    ( supplier_id                     ),\n" +
+                "    (              product_id, rating ),\n" +
+                "    (              product_id         ),\n" +
+                "    (                          rating ),\n" +
+                "    (                                 ))";
+        tableEnvironment.executeSql(equalCubeGroupingSql2).print();
+    }
+
+    /**
+     * SELECT
+     *   agg_func(agg_col) OVER (
+     *     [PARTITION BY col1[, col2, ...]]
+     *     ORDER BY time_col
+     *     range_definition),
+     *   ...
+     * FROM ...
+     * @param tableEnvironment
+     */
+    private static void overAgg(StreamTableEnvironment tableEnvironment){
+        tableEnvironment.executeSql(expample_sql);
+        // 以下RANGE间隔定义了将所有time属性小于当前行最多5 s的行包含在聚合中。
+        String overSql="select id,sum(price) over w as sum_price from test" +
+                " WINDOW w AS ( PARTITION by id order by event_time RANGE BETWEEN INTERVAL '5' second PRECEDING AND " +
+                "CURRENT ROW)";
+        tableEnvironment.executeSql(overSql).print();
+    }
+
 }
