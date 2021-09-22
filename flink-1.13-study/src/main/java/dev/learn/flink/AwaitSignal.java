bf19996427bd898ae2f6fb563896d0424e5dc1c6
Shimin Huang <40719512+collabH@users.noreply.github.com>
2021-09-22 20:31:31 +0800

    feat: chm

---
+++ flink-1.13-study/src/main/java/dev/learn/flink/AwaitSignal.java
@@ -0,0 +1,52 @@
+package dev.learn.flink;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AwaitSignal {
+    private static ReentrantLock lock = new ReentrantLock();
+    private static Condition condition = lock.newCondition();
+    private static volatile boolean flag = false;
+
+    public static void main(String[] args) {
+        Thread waiter = new Thread(new waiter());
+        waiter.start();
+        Thread signaler = new Thread(new signaler());
+        signaler.start();
+    }
+
+    static class waiter implements Runnable {
+
+        @Override
+        public void run() {
+            lock.lock();
+            try {
+                while (!flag) {
+                    System.out.println(Thread.currentThread().getName() + "当前条件不满足等待");
+                    try {
+                        condition.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(Thread.currentThread().getName() + "接收到通知条件满足");
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    static class signaler implements Runnable {
+
+        @Override
+        public void run() {
+            lock.lock();
+            try {
+                flag = true;
+                condition.signalAll();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
