0a2abb6305d5d168726e7a0ba19d1f17216d35ca
Shimin Huang <40719512+collabH@users.noreply.github.com>
2021-10-31 21:31:26 +0800

    feat[hudi]: cdc insert hudi

--- flink1.12-study/src/main/java/dev/learn/flink/hudi/CDC2Hudi.java
+++ flink1.12-study/src/main/java/dev/learn/flink/hudi/CDC2Hudi.java
@@ -1,5 +1,9 @@
 package dev.learn.flink.hudi;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
 /**
  * @fileName: CDC2Hudi.java
  * @description: flink cdc sink hudi
@@ -7,7 +11,39 @@ package dev.learn.flink.hudi;
  * @date: 2021/10/30 8:07 下午
  */
 public class CDC2Hudi {
-    private static String getCDCMysqlSource(){
-        return "create table cdc_source()with()";
+    private static String getCDCMysqlSource() {
+        return "create table cdc_source(" +
+                "id int," +
+                "username string," +
+                "age int," +
+                "primary key(id) not enforced)with(" +
+                "'connector'='mysql-cdc'," +
+                "'hostname'='hadoop'," +
+                "'username'='root'," +
+                "'port'='3306'," +
+                "'password'='root'," +
+                "'database-name'='cdc'," +
+                "'table-name'='user')";
+    }
+
+    private static String getHudiSink() {
+        return "create table hudi_sink(" +
+                "id int," +
+                "username string," +
+                "age int)PARTITIONED BY (id)with(" +
+                "'connector'='hudi'," +
+                "'path'='hdfs:///user/hudi/user'," +
+                "'hoodie.datasource.write.recordkey.field'='id'," +
+                "'write.precombine.field'='username')";
+    }
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env,
+                EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build());
+        tableEnv.executeSql(getCDCMysqlSource());
+        tableEnv.executeSql(getHudiSink());
+
+        tableEnv.executeSql("insert into hudi_sink select * from cdc_source").print();
     }
 }
