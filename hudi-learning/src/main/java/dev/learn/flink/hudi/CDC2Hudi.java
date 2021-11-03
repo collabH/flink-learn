125595244cc85e6b49ee27c95dfe619fb44d24a5
Shimin Huang <40719512+collabH@users.noreply.github.com>
2021-11-03 19:55:55 +0800

    feat(hudi):cdc sink hudi

--- hudi-learning/src/main/java/dev/learn/flink/hudi/CDC2Hudi.java
+++ hudi-learning/src/main/java/dev/learn/flink/hudi/CDC2Hudi.java
@@ -1,8 +1,11 @@
 package dev.learn.flink.hudi;
 
+import org.apache.flink.streaming.api.environment.ExecutionCheckpointingOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import java.time.Duration;
 
 /**
  * @fileName: CDC2Hudi.java
@@ -12,38 +15,47 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
  */
 public class CDC2Hudi {
     private static String getCDCMysqlSource() {
-        return "create table cdc_source(" +
-                "id int," +
-                "username string," +
-                "age int," +
-                "primary key(id) not enforced)with(" +
+        return "create table order_source(" +
+                "order_number int," +
+                "order_date string," +
+                "purchaser int," +
+                "quantity int," +
+                "product_id int," +
+                "primary key(order_number) not enforced)with(" +
                 "'connector'='mysql-cdc'," +
-                "'hostname'='hadoop'," +
+                "'hostname'='localhost'," +
                 "'username'='root'," +
                 "'port'='3306'," +
-                "'password'='root'," +
-                "'database-name'='cdc'," +
-                "'table-name'='user')";
+                "'password'='123456'," +
+                "'database-name'='inventory'," +
+                "'server-id'='101'," +
+                "'table-name'='order')";
     }
 
     private static String getHudiSink() {
-        return "create table hudi_sink(" +
-                "id int," +
-                "username string," +
-                "age int)PARTITIONED BY (id)with(" +
+        return "create table order_sink(" +
+                "order_number int," +
+                "order_date string," +
+                "purchaser int," +
+                "quantity int," +
+                "product_id int)" +
+                "PARTITIONED BY (order_number)with(" +
                 "'connector'='hudi'," +
-                "'path'='hdfs:///user/hudi/user'," +
-                "'hoodie.datasource.write.recordkey.field'='id'," +
-                "'write.precombine.field'='username')";
+                "'path'='hdfs:///user/hudi/order'," +
+                "'hoodie.datasource.write.recordkey.field'='order_number'," +
+                "'write.precombine.field'='order_date')";
     }
 
     public static void main(String[] args) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env,
                 EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build());
+        tableEnv.getConfig().getConfiguration().set(ExecutionCheckpointingOptions.CHECKPOINTING_INTERVAL,
+                Duration.ofSeconds(3));
         tableEnv.executeSql(getCDCMysqlSource());
         tableEnv.executeSql(getHudiSink());
 
-        tableEnv.executeSql("insert into hudi_sink select * from cdc_source").print();
+        tableEnv.executeSql("insert into order_sink select * from order_source");
+//        tableEnv.executeSql("select * from order_source").print();
     }
 }
