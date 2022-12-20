604db0d65fc15b890b219ba0042538e434988c82
min <40719512+collabH@users.noreply.github.com>
2022-12-20 20:42:35 +0800

    feat: table api

---
+++ flink-1.13-study/src/main/java/dev/learn/flink/sql/table/TableApiDemo.java
@@ -0,0 +1,44 @@
+package dev.learn.flink.sql.table;
+
+import org.apache.flink.connector.datagen.table.DataGenConnectorOptions;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Schema;
+import org.apache.flink.table.api.TableDescriptor;
+import org.apache.flink.table.api.TableEnvironment;
+
+/**
+ * @fileName: TableApiDemo.java
+ * @description: table api demo
+ * @author: huangshimin
+ * @date: 2022/12/20 7:04 PM
+ */
+public class TableApiDemo {
+    public static void main(String[] args) {
+        TableEnvironment tableEnv = TableEnvironment.create(EnvironmentSettings.newInstance()
+                .inStreamingMode()
+                .build());
+
+        tableEnv.createTemporaryTable("tempSourceTable", TableDescriptor.forConnector("datagen")
+                .schema(Schema.newBuilder()
+                        .column("id", DataTypes.INT())
+                        .column("name", DataTypes.STRING())
+                        .build())
+                .option(DataGenConnectorOptions.ROWS_PER_SECOND, 100L)
+                .build());
+
+        // 创建blackhole sink
+        tableEnv.executeSql("CREATE TEMPORARY TABLE SinkTable WITH ('connector' = 'blackhole') LIKE tempSourceTable " +
+                "(EXCLUDING OPTIONS) ");
+
+        // insert table
+//        tableEnv.from("tempSourceTable").insertInto("SinkTable").execute();
+
+
+        // 创建虚拟表
+        tableEnv.createTemporaryView("tempSourceView",tableEnv.from("tempSourceTable"));
+        
+        // register udf
+        tableEnv.createFunction("split_str", );
+    }
+}
---
+++ flink-1.13-study/src/main/java/dev/learn/flink/sql/table/udf/SplitStrUdf.java
@@ -0,0 +1,24 @@
+package dev.learn.flink.sql.table.udf;
+
+import org.apache.flink.table.catalog.DataTypeFactory;
+import org.apache.flink.table.functions.FunctionKind;
+import org.apache.flink.table.functions.UserDefinedFunction;
+import org.apache.flink.table.types.inference.TypeInference;
+
+/**
+ * @fileName: SplitStrUdf.java
+ * @description: SplitStrUdf.java类说明
+ * @author: huangshimin
+ * @date: 2022/12/20 8:08 PM
+ */
+public class SplitStrUdf extends UserDefinedFunction {
+    @Override
+    public FunctionKind getKind() {
+        return null;
+    }
+
+    @Override
+    public TypeInference getTypeInference(DataTypeFactory dataTypeFactory) {
+        return null;
+    }
+}
